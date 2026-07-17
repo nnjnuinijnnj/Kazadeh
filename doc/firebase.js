eb3f0b31a92d8149c5420882c609d342a4d6d0ac
nnjnuinijnnj <abzartajhiz@gmail.com>
2026-07-17 17:38:32 +0330

    Update firebase.js

--- doc/firebase.js
+++ doc/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKhoGmV-8BeXijxbweqW3suhmI4rGMIRk",
+  apiKey: "AIzaSyCKhoGmV-8-BeXijxbweqW3suhmI4rGMIRk",
   authDomain: "kazadeh-online.firebaseapp.com",
   projectId: "kazadeh-online",
   storageBucket: "kazadeh-online.firebasestorage.app",
@@ -13,11 +10,6 @@ const firebaseConfig = {
   appId: "1:1066989049488:web:3dbf308460ebd52c71ea15",
   measurementId: "G-R42LJEJBK5"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const app = initializeApp(firebaseConfig);
 
